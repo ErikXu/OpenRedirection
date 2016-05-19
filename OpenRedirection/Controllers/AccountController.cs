760166829ac20ee1311acd9f0ba2131d22bce79e
Erik Xu <81636759@qq.com>
2016-05-19 15:56:58 +0800

    Init.

---
+++ OpenRedirection/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using OpenRedirection.Models;
+
+namespace OpenRedirection.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult LogOn()
+        {
+            return View(new LogOnModel());
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            //Your logon logic here.
+
+            FormsAuthentication.SetAuthCookie(model.UserName, false);
+
+            if (!string.IsNullOrEmpty(returnUrl)
+                && Url.IsLocalUrl(returnUrl) //Comment out this code will cause open redirection 
+                )
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("LogOn", "Account");
+        }
+    }
+}
---
+++ OpenRedirection/Models/LogOnModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenRedirection.Models
+{
+    public class LogOnModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+ 
+    }
+}
